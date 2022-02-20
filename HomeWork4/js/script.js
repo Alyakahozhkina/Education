aa658f56c734f11f5c534fe13a19aba15f3788e8
Alina Finko <alya.finko@gmail.com>
2022-02-21 01:39:45 +0200

    Done Bootstrap task

---
+++ HomeWork4/js/script.js
@@ -0,0 +1,18 @@
+//BUTTON-UP
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+//TOOLTIPS
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
